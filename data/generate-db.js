8b914693c7f43cb313e88c68d14034750274a2b7
Yurim Jin <jayjinjay@gmail.com>
2023-03-26 16:32:50 +0900

    companies를 key로해서 넣도록

--- data/generate-db.js
+++ data/generate-db.js
@@ -5,10 +5,6 @@ const matter = require("gray-matter");
 const companiesDir = path.join(process.cwd(), "companies");
 const rootDir = path.join(process.cwd(), "..");
 
-// if (!fs.existsSync(buildDir)) {
-//   fs.mkdirSync(buildDir);
-// }
-
 const files = fs.readdirSync(companiesDir);
 const listPath = path.join(rootDir, "db.json");
 const list = [];
@@ -17,11 +13,6 @@ files.forEach((fileName) => {
   const filePath = path.join(companiesDir, fileName);
   const fileContents = fs.readFileSync(filePath, "utf8");
   const { data, content } = matter(fileContents);
-
-  // const jsonFileName = `${path.parse(fileName).name}.json`;
-  // const jsonFilePath = path.join(buildDir, jsonFileName);
-
-  // fs.writeFileSync(jsonFilePath, JSON.stringify({ ...data, content }));
   list.push({ id: fileName, ...data, content });
 });
-fs.writeFileSync(listPath, JSON.stringify(list));
+fs.writeFileSync(listPath, JSON.stringify({ companies: list }));
